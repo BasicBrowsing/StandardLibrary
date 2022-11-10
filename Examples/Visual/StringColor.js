b4bc850a526f808bd870c5c45357e06e63cc7f04
LowerDimensions <LowerDimensions@gmail.com>
2022-11-10 11:08:19 -0500

    Added color preview for string color.

--- Examples/Visual/StringColor.js
+++ Examples/Visual/StringColor.js
@@ -1,11 +1,11 @@
 
 import { rgbFrom , hexFrom } from '../../Source/Visual/StringColor.js'
-
+import { bgRgb24 , bgWhite } from 'Colors'
 
 const { log } = console;
 
 
-const string = '106.0.0.0';
+const string = '106.0.0.1';
 
 const
     rgb = await rgbFrom(string) ,
@@ -16,5 +16,10 @@ log(`
     String : ${ string }
     RGB : ${ rgb.r } ${ rgb.g } ${ rgb.b }
     HEX : ${ hex }
+    Preview :
+    
+    ${ bgWhite('          ') }
+    ${ bgWhite('    ') }${ bgRgb24('  ',rgb) }${ bgWhite('    ')}
+    ${ bgWhite('          ') }
     
 `);
